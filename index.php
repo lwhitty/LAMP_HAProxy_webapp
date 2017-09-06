02ad186bce729b50cb2333c6240bcf8bc8a4d546
lwhitty <lee.whitty@gmail.com>
2017-09-06 11:33:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
 <header>
-     <h1>Hello World! LAMP Stack and HA Proxy Load Balancer deployed via Ansible Tower.</h1>
-     <h1>Web Server Hostname is: <?php $host= gethostname(); echo $host; ?> </h1>
+     <h1>LAMP Stack and HA Proxy Load Balancer deployed via Ansible Tower.</h1>
+     <h1>Web Server Hostname is: <style> body { color: blue } </style> <?php $host= gethostname(); echo $host; ?> </h1>
 </html>
