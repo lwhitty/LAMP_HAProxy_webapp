6138470c5cf8e5bf1ca02e333aefb3b2e538b64e
lwhitty <lee.whitty@gmail.com>
2017-09-06 10:45:28 -0400

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
 <header>
      <h1>Hello World! LAMP Stack and HA Proxy Load Balancer deployed via Ansible Tower.</h1>
-     <h1>IP Address = {{ ansible_default_ipv4.address }} </h1>
+     <h1>Web Server IP address is: <? echo $_SERVER["REMOTE_ADDR"]; ?> </h1>
 </html>
